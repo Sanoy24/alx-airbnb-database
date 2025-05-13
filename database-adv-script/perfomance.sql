addee9c04f57c15379a5db2bc2c8ec09c272b71b
Yonas Mekonnen <myonas886@gmail.com>
2025-05-13 11:23:50 +0300

    Refactor complex queries to improve performance.

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -55,20 +55,27 @@ LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
 --  =======================================
 --      OPTIMIZED VERSION OF THE QUERY
 --  =======================================
-SELECT
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.total_price,
-    b.status,
-    u.first_name,
-    u.last_name,
-    p.name AS property_name,
-    p.location,
-    pay.amount,
-    pay.payment_method
-FROM bookings b
-JOIN users u ON b.user_id = u.user_id
-JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+ SELECT
+     b.booking_id,
+     b.start_date,
+     b.end_date,
+     b.total_price,
+     b.status,
+     u.first_name,
+     u.last_name,
+     u.email,
+     p.name AS property_name,
+     p.location,
+     p.pricepernight,
+     pay.payment_id,
+     pay.amount,
+     pay.payment_method,
+     pay.payment_date
+ FROM bookings b
+ JOIN users u ON b.user_id = u.user_id
+ JOIN properties p ON b.property_id = p.property_id
+ LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+ WHERE b.status = 'confirmed'
+   AND b.start_date >= CURRENT_DATE
+   AND p.location = 'New York';
 
