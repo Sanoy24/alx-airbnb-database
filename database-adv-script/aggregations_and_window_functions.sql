53e76815f59731f3c509e766d9d5f03f00048e58
Yonas Mekonnen <myonas886@gmail.com>
2025-05-13 10:51:21 +0300

    updateed the window function query to include ROU_NUMBER clause

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -33,7 +33,8 @@ SELECT
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
 FROM 
     properties p
 LEFT JOIN 
@@ -42,4 +43,5 @@ GROUP BY
     p.property_id, p.name
 ORDER BY 
     booking_rank;
+
 
