310d2f66f725b5daac018b3a021f7a704f96163e
Yonas Mekonnen <myonas886@gmail.com>
2025-05-13 09:31:56 +0300

    updated the left join query to include oerder by clause using rating

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -5,6 +5,6 @@
 
 SELECT * FROM bookings INNER JOIN users USING(user_id)
 
-SELECT * FROM properties LEFT JOIN reviews USING(property_id)
+SELECT * FROM properties LEFT JOIN reviews USING(property_id) ORDER BY reviews.rating DESC
 
 SELECT * FROM users FULL OUTER JOIN bookings USING(user_id)
